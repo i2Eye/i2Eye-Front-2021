b84e7b175ab6f7d7c0608c38d91d62f832723b8d
Felicia Ivane <e0552298@u.nus.edu>
2021-12-30 17:21:29 +0700

    Display registration in update forms page and add paddings

--- src/Components/StationComponents/EditForms.js
+++ src/Components/StationComponents/EditForms.js
@@ -1,28 +1,24 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Paper from '@material-ui/core/Paper'
-import TextField from '@material-ui/core/TextField';
-import AddCircle from '@material-ui/icons/AddCircle';
-import RemoveCircle from '@material-ui/icons/RemoveCircle';
-import IconButton from '@material-ui/core/IconButton';
+import * as React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import AddCircle from "@material-ui/icons/AddCircle";
+import RemoveCircle from "@material-ui/icons/RemoveCircle";
+import IconButton from "@material-ui/core/IconButton";
 import List from "@material-ui/core/List";
-import Button from '@material-ui/core/Button';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+import Button from "@material-ui/core/Button";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
 import Data from "./StationTestData.json";
 import ListItemText from "@material-ui/core/ListItemText";
 import { Link } from "react-router-dom";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-
-import Switch from "@material-ui/core/Switch";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -53,7 +49,7 @@ TabPanel.propTypes = {
 function a11yProps(index) {
   return {
     id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
   };
 }
 
@@ -64,27 +60,44 @@ function AddFormTab() {
     return (e) => {
       questions[qnIndex] = e.target.value;
     };
-  }
+  };
 
-  const addQuestion = () => setQuestions((prev)=>{
-    prev.forEach((str) => console.log(str));
-    return [...prev, ""];
-  });
-  const removeQuestion = (qnIndex) => () => setQuestions((prev)=> {
-    prev.forEach((str) => console.log(str));
-    return prev.filter((qn, index) => index !== qnIndex)
-  });
-  
+  const addQuestion = () =>
+    setQuestions((prev) => {
+      prev.forEach((str) => console.log(str));
+      return [...prev, ""];
+    });
+  const removeQuestion = (qnIndex) => () =>
+    setQuestions((prev) => {
+      prev.forEach((str) => console.log(str));
+      return prev.filter((qn, index) => index !== qnIndex);
+    });
+
   function InputField(props) {
     return (
       <ul>
         <div>
-          <TextField size="small" style ={{width: '80%', marginTop:'10px'}} required id="outlined-basic" label="Question" variant="outlined" onChange={handleTextChange(props.index)} />
-          <IconButton onClick={addQuestion}><AddCircle /></IconButton>
-          <IconButton onClick={removeQuestion(props.index)} disabled={questions.length === 1}><RemoveCircle /></IconButton>
+          <TextField
+            size="small"
+            style={{ width: "80%", marginTop: "10px" }}
+            required
+            id="outlined-basic"
+            label="Question"
+            variant="outlined"
+            onChange={handleTextChange(props.index)}
+          />
+          <IconButton onClick={addQuestion}>
+            <AddCircle />
+          </IconButton>
+          <IconButton
+            onClick={removeQuestion(props.index)}
+            disabled={questions.length === 1}
+          >
+            <RemoveCircle />
+          </IconButton>
         </div>
       </ul>
-    )
+    );
   }
 
   return (
@@ -98,13 +111,31 @@ function AddFormTab() {
     >
       <List>
         <ul>
-          <TextField required id="outlined-basic" label="Form Name" variant="outlined" /><br></br>
+          <TextField
+            required
+            id="outlined-basic"
+            label="Form Name"
+            variant="outlined"
+          />
+          <br></br>
         </ul>
-        {questions.map((question, index) => {return <InputField question={question} index={index} />})}
+        {questions.map((question, index) => {
+          return <InputField question={question} index={index} />;
+        })}
       </List>
-      <Button style ={{background: '#2B6AE2', margin: '40px', float: 'right', color: 'white'}} variant="contained">Submit</Button>
+      <Button
+        style={{
+          background: "#2B6AE2",
+          margin: "40px",
+          float: "right",
+          color: "white",
+        }}
+        variant="contained"
+      >
+        Submit
+      </Button>
     </Paper>
-  )
+  );
 }
 
 function UpdateFormTab() {
@@ -119,9 +150,17 @@ function UpdateFormTab() {
         checked: false, // Availabilities unimportant here, set to false.
       });
     }
-  }
+  };
+
+  const registration = {
+    stationName: "Registration",
+    tag: "registration",
+    available: "false",
+  };
 
   getStations();
+
+  var i;
 
   return (
     <Paper
@@ -129,26 +168,54 @@ function UpdateFormTab() {
         paddingTop: 20,
         paddingLeft: 30,
         paddingRight: 30,
-        paddingBottom: 20,
+        paddingBottom: 10,
       }}
     >
-        <List>
-          {stations.map((station, index) => (
-            <ul
-              key={index}
-              component={Link}
-              to={`/edit_forms/${station.tag}`}
-            >
-              <ListItemText id={station.name} primary={station.name} />
-            </ul>
-          ))}
-        </List>
-      </Paper>
-  )
+      <FormLabel component="legend">Click form to edit</FormLabel>
+      <List>
+        {stations.map(
+          (station, index) => (
+            (i = index),
+            (
+              <li
+                style={{
+                  paddingTop: 10,
+                }}
+                key={index}
+                component={Link}
+                to={`/edit_forms/stations`}
+              >
+                <ListItemText id={station.name} primary={station.name} />
+              </li>
+            )
+          )
+        )}
+        <li
+          style={{
+            paddingTop: 10,
+          }}
+          key={i + 1}
+          component={Link}
+          to={`/edit_forms/${registration.tag}`}
+        >
+          <ListItemText
+            id={registration.stationName}
+            primary={registration.stationName}
+          />
+        </li>
+      </List>
+    </Paper>
+  );
 }
 
 function DropFormTab() {
-  const forms = ["Registration", "BMI", "Doctor Consultation", "Familty History", "Oral Health"];
+  const forms = [
+    "Registration",
+    "BMI",
+    "Doctor Consultation",
+    "Familty History",
+    "Oral Health",
+  ];
   // const formInit = {};
   // forms.forEach((formName) => formInit[formName] = false);
   let formsToDelete = [];
@@ -165,20 +232,20 @@ function DropFormTab() {
     if (event.target.checked) {
       formsToDelete.push(event.target.name);
     } else {
-      formsToDelete = formsToDelete.filter((form) => form !== event.target.name);
+      formsToDelete = formsToDelete.filter(
+        (form) => form !== event.target.name
+      );
     }
     formsToDelete.forEach((name) => console.log(name));
   };
 
   function FormCheckBox(props) {
     return (
-      <FormControlLabel 
-        control={
-          <Checkbox onChange={handleChange} name={props.name} />
-        }
+      <FormControlLabel
+        control={<Checkbox onChange={handleChange} name={props.name} />}
         label={props.name}
       />
-    )
+    );
   }
 
   return (
@@ -193,12 +260,24 @@ function DropFormTab() {
       <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
         <FormLabel component="legend">Select forms to drop</FormLabel>
         <FormGroup>
-          {forms.map((form) => {return <FormCheckBox name={form} />})}
+          {forms.map((form) => {
+            return <FormCheckBox name={form} />;
+          })}
         </FormGroup>
       </FormControl>
-      <Button style ={{background: '#2B6AE2', margin: '40px', float: 'right', color: 'white'}} variant="contained">Submit</Button>
+      <Button
+        style={{
+          background: "#2B6AE2",
+          margin: "40px",
+          float: "right",
+          color: "white",
+        }}
+        variant="contained"
+      >
+        Submit
+      </Button>
     </Paper>
-  )
+  );
 }
 
 export default function BasicTabs() {
@@ -210,15 +289,19 @@ export default function BasicTabs() {
 
   return (
     <div>
-      <Box sx={{ width: '100%', borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+      <Box sx={{ width: "100%", borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
           <Tab label="Add" {...a11yProps(0)} />
           <Tab label="Update" {...a11yProps(1)} />
           <Tab label="Drop" {...a11yProps(2)} />
         </Tabs>
       </Box>
       <h1>Edit Forms</h1>
-      <Box sx={{ width: '100%' }}>
+      <Box sx={{ width: "100%" }}>
         <TabPanel value={value} index={0}>
           <AddFormTab />
         </TabPanel>
